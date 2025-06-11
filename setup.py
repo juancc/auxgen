26e09ef05090e8fcacab788eb9cf051a9312a796
juancc <jkaz.os@gmail.com>
2025-06-11 10:33:32 -0500

    Added setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='auxgen',
+    version='0.0.1',    
+    description='Auxiliary functions for Generative design',
+    url='https://github.com/juancc/auxgen',
+    author='Juan Carlos Arbelaez',
+    author_email='jarbel16@eafit.edu.co',
+    license= '',
+    packages=['auxgen'],
+    install_requires=[
+                      'tqdm',
+                      'numpy',
+                      'matplotlib',
+                      'numpy-stl'                     
+                      ],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
