12cc7a82b0ff5b24ca4139a5f1883d6eb1184d78
juancc <jkaz.os@gmail.com>
2025-07-09 08:08:18 -0500

    Added get random designs from list

--- auxgen/auxfunc.py
+++ auxgen/auxfunc.py
@@ -5,6 +5,7 @@ JCA
 """
 import os
 import json
+import random
 
 import pandas as pd
 from tqdm import tqdm
@@ -37,6 +38,30 @@ def get_best_designs(designs, costs, n):
     sorted_pairs = sorted(zip(costs, designs), key=lambda x: x[0])
     best_costs, best_designs = zip(*sorted_pairs[:n])
     return list(best_designs), list(best_costs)
+
+
+def get_random_designs(costs, designs, n, seed=None):
+    """
+    Return `n` random designs and their associated costs.
+
+    Parameters:
+        costs (list of float): List of cost values.
+        designs (list of list): List of corresponding design parameters.
+        n (int): Number of random designs to return.
+        seed (int, optional): Random seed for reproducibility.
+
+    Returns:
+        tuple: (random_costs, random_designs), both lists of length `n`.
+    """
+    if seed is not None:
+        random.seed(seed)
+
+    indices = random.sample(range(len(costs)), n)
+    random_costs = [costs[i] for i in indices]
+    random_designs = [designs[i] for i in indices]
+    
+    return random_costs, random_designs
+
 
 
 def save_models_from_list(designs:list, build_model, save_path:str, format='stl'):
